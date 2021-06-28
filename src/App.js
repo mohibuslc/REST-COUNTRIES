6094b8494127c3f477fe6f3c0f1eadca830606ff
mohibuslc <mohib_uslc@outlook.com>
2021-06-28 22:50:03 +0100

    REST-countries API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect} from 'react';
 
 function App() {
+
+  const [countries , setCountries]=useState([]);
+  useEffect(()=>{
+
+
+
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then (data => setCountries(data))
+  },[]);
   return (
+
+   
     <div className="App">
+
+      <h1>Total Countries Information : {countries.length}</h1>
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
